32636409ec3d63f4fe791d9446bb034d9ef32697
Nicholas Khorasani <nckhoras@uwaterloo.ca>
2020-12-24 00:52:37 -0500

    Added post request

--- spotify_controller/api/models.py
+++ spotify_controller/api/models.py
@@ -12,7 +12,7 @@ def generate_unique_code():
         # will generate a random code only using upper case ascii characters
         code = ''.join(random.choices(string.ascii_uppercase, k=lenght))
         # room.objects gives me all the room objects
-        if Room.objects.fiter(code=code).count() == 0:
+        if Room.objects.filter(code=code).count() == 0:
             break
 
     return code
@@ -23,7 +23,8 @@ class Room(models.Model):
     # you can then use python code to access the stuff
 
     # the arguments are the constrains on the code field.
-    code = models.CharField(max_length=8, default="", unique=True)
+    code = models.CharField(
+        max_length=8, default=generate_unique_code, unique=True)
 
     # store information about the host
     host = models.CharField(max_length=50, unique=True)
@@ -38,3 +39,11 @@ class Room(models.Model):
 
     # you can add methods on the models
     # in Django we want FAT models and thin views
+
+# A session is a temp connection between two devices
+# A page that you would usually have to sign into but you dont have to
+# because its the same device
+
+# I dont have to sign into facebook each time because its still in the same session
+
+# sessions have to do with a unique key
--- spotify_controller/api/serializers.py
+++ spotify_controller/api/serializers.py
@@ -10,3 +10,10 @@ class RoomSerializer(serializers.ModelSerializer):
         # on each model their is a primary key (a unique integer) that is automatically created when you add a new model to the database
         fields = ('id', 'code', 'host', 'guest_can_pause',
                   'votes_to_skip', 'created_at')
+
+
+# To send a post request with a 'payload'
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
